c65240e6644efeecaf84a29ce47d24eb6a01bf70
LorandSa <lorand.sabau@yahoo.com>
2022-07-17 16:35:48 +0300

    Tema Lab 14

--- src/AnimalRescuer/Duck.java
+++ src/AnimalRescuer/Duck.java
@@ -5,4 +5,7 @@ public class Duck extends Animal{
     public void speak(){
         System.out.println("Quack quack ");
     }
+    public void eat(){
+        System.out.println("eating duck food");
+    }
 }
--- src/AnimalRescuer/Mouse.java
+++ src/AnimalRescuer/Mouse.java
@@ -4,4 +4,7 @@ public class Mouse extends Animal{
     public void speak(){
         System.out.println("Squeak");
     }
+    public void eat(){
+        System.out.println("eating mouse food");
+    }
 }
